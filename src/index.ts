55f14936e5d412a389649ae03e130db09dc66664
Aashim Limbu <thelastmanarmy37@gmail.com>
2024-07-02 13:11:59 +0545

    initialize the cloudflare workers

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,10 @@
 import { PrismaClient } from '@prisma/client/edge';
 import { withAccelerate } from '@prisma/extension-accelerate';
+
+const prisma = new PrismaClient().$extends(withAccelerate());
 export default {
 	async fetch(request, env, ctx): Promise<Response> {
-		const prisma = new PrismaClient().$extends(withAccelerate());
-		const user = await prisma.user.findMany();
+		const user = await prisma.user.findMany({ cacheStrategy: { swr: 10, ttl: 30 } });
 		return new Response(JSON.stringify({ user }), {
 			headers: { 'Content-Type': 'application/json' },
 		});
